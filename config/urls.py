bf366de51f3957b85ad840728e171b361cbf5424
Eduardo Rojas <eduardobadillorojas@gmail.com>
2026-08-01 14:20:55 -0600

    Corregida importacion de views en config urls

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
+from django.contrib import admin
 from django.urls import path
-from . import views
+from core import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.inicio, name='inicio'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
@@ -9,4 +11,5 @@ urlpatterns = [
     path('radiologia/', views.panel_radiologo, name='panel_radiologo'),
     path('recepcion/', views.panel_recepcion, name='panel_recepcion'),
     path('configuracion/', views.panel_config, name='panel_config'),
+]
 ]
