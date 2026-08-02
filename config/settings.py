a87d845036d7aa28aaac69ce100c4f5d663ac92a
Eduardo Rojas <eduardobadillorojas@gmail.com>
2026-08-01 18:10:39 -0600

    Configurar STATIC_ROOT para Render

--- config/settings.py
+++ config/settings.py
@@ -123,5 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
